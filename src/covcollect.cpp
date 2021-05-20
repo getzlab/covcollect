b48a7dabb9978d94e8dd0605719757c6b6b87411
Claudia Chu <cchu@broadinstitute.org>
2021-05-20 17:22:54 -0500

    indexing

--- src/covcollect.cpp
+++ src/covcollect.cpp
@@ -79,7 +79,7 @@ bool cc_walker::walk_apply(const SeqLib::BamRecord& record) {
 	}
 
 	// Add bins
-	for(uint64_t i = binmax; i < record.PositionEnd() + binwidth; i = i + binwidth) {
+	for(uint64_t i = binmax + binwidth; i < record.PositionEnd() + binwidth; i = i + binwidth) {
 		active_bins.emplace(i, (target_counts_t){0, 0});
 		binmax = i;
 	}
