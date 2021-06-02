1058a30c9cff51b574bd19961e8dc13fbd327c48
cchu70 <cchu@broadinstitute.org>
2021-06-02 20:12:40 +0000

    separate class for bin version

--- src/covcollect.cpp
+++ src/covcollect.cpp
@@ -16,21 +16,105 @@ void cc_walker::load_intervals(uint32_t pad) {
    this->pad = pad;
 }
 
-void cc_walker::set_binwidth(uint32_t binwidth) {
+void cc_walker::walk_all() {
+   cur_region = intervals[0];
+   walker::walk(intervals);
+}
+
+uint32_t cc_walker::n_overlap(const SeqLib::GenomicRegion& region, uint32_t start, uint32_t end) {
+   if(start > region.pos2 - this->pad || region.pos1 + this->pad > end) return 0;
+   return MIN(region.pos2 - this->pad, end) - MAX(start, region.pos1 + this->pad);
+}
+
+bool cc_walker::walk_apply(const SeqLib::BamRecord& record) {
+   std::string read_name = record.Qname();
+
+   // if we've switched regions, flush the cache and write current counts
+   size_t region_idx = reader.GetRegionIdx();
+   if(region_idx != cur_region_idx) {
+      // any reads left in the cache will all be singletons
+      for(const auto& read : read_cache) {
+	 target_coverage.n_corrected += n_overlap(cur_region, read.second.start, read.second.end);
+	 target_coverage.n_uncorrected += n_overlap(cur_region, read.second.start, read.second.end);
+      }
+      read_cache.clear();
+
+      fprintf(outfile, "%s\t%d\t%d\t%d\t%d\n",
+        header.IDtoName(cur_region.chr).c_str(),
+        cur_region.pos1 + this->pad,
+        cur_region.pos2 - this->pad,
+        target_coverage.n_corrected,
+        target_coverage.n_uncorrected
+      );
+      target_coverage = {0, 0};
+
+      // we may have skipped over multiple empty regions
+      for(size_t r = cur_region_idx + 1; r < region_idx; r++) {
+	 SeqLib::GenomicRegion gr = intervals[r];
+	 fprintf(outfile, "%s\t%d\t%d\t%d\t%d\n",
+	   header.IDtoName(gr.chr).c_str(),
+	   gr.pos1 + this->pad,
+	   gr.pos2 - this->pad,
+	   0,
+	   0
+	 );
+      }
+
+      // switch to next region
+      cur_region = intervals[region_idx];
+      cur_region_idx = region_idx;
+   }
+
+   // this is the first read in the pair; push to cache
+   if(read_cache.find(read_name) == read_cache.end()) {
+      read_cache.emplace(
+        read_name,
+        (read_boundary_t) {
+	  (uint32_t) record.Position(),
+	  (uint32_t) record.PositionEnd()
+        }
+      );
+
+   // this is the second read in the pair; lookup first read in cache to see
+   // if it overlaps
+   } else {
+      // if the reads overlap, consider them as a single entity
+      if(MAX(0, (int32_t) read_cache[read_name].end - (int32_t) record.Position()) > 0) {
+	 target_coverage.n_corrected += n_overlap(cur_region, read_cache[read_name].start, record.PositionEnd());
+
+      // otherwise, consider their contributions separately
+      } else {
+	 target_coverage.n_corrected += n_overlap(cur_region, read_cache[read_name].start, read_cache[read_name].end) + 
+           n_overlap(cur_region, record.Position(), record.PositionEnd());
+      }
+
+      // save coverage that doesn't account for overlaps for demo purposes
+      target_coverage.n_uncorrected += n_overlap(cur_region, read_cache[read_name].start, read_cache[read_name].end) + 
+	n_overlap(cur_region, record.Position(), record.PositionEnd());
+
+      // remove from cache
+      read_cache.erase(read_name);
+   }
+
+   return 1;
+}
+
+
+void cc_bin_walker::set_binwidth(uint32_t binwidth) {
 	this->binwidth = binwidth;
 }
 
-void cc_walker::walk_all() {
+void cc_bin_walker::walk_all() {
    walker::walk();
 }
 
-uint32_t cc_walker::n_overlap(const uint32_t binstart, uint32_t binend, uint32_t start, uint32_t end) {
+uint32_t cc_bin_walker::n_overlap(const uint32_t binstart, uint32_t binend, uint32_t start, uint32_t end) {
    if(start > binstart + this->binwidth || binstart > end) return 0;
    return MIN(binend, end) - MAX(start, binstart);
 }
 
 
-bool cc_walker::walk_apply(const SeqLib::BamRecord &record) {
+bool cc_bin_walker::walk_apply(const SeqLib::BamRecord &record) {
 	std::string read_name = record.Qname();
 	int32_t record_chr = record.ChrID();
 
@@ -129,11 +213,29 @@ int main(int argc, char** argv) {
    walker::basic_arg_t args = {};
    if(!walker::basic_argparse(argc, argv, &args)) exit(1);
 
-   CC::cc_walker w = CC::cc_walker(args.bam_in, args.input_file);
-   w.set_binwidth(args.bin_width);
-   if(!w.set_output_file(args.output_file)) exit(1);
+   bool use_bins = true;
 
-   w.walk_all();
+   for (int i=0; i < args.input_file.length(); i++) {
+      if (isdigit(args.input_file[i]) == false) {
+         use_bins = false;
+         break;
+      }
+   }
+
+   std::ifstream is_file(args.input_file);
+
+   if (use_bins) {
+      CC::cc_bin_walker  w = CC::cc_bin_walker(args.bam_in, stoi(args.input_file));   
+      if(!w.set_output_file(args.output_file)) exit(1);
+      w.walk_all();
+   } else if (is_file) {
+      CC::cc_walker w = CC::cc_walker(args.bam_in, args.input_file);
+      w.load_intervals(151);
+      if(!w.set_output_file(args.output_file)) exit(1);
+      w.walk_all();
+   } else {
+      exit(1);
+   }
 
    return 0;
 }
--- src/covcollect.hpp
+++ src/covcollect.hpp
@@ -25,9 +25,8 @@ class cc_walker : public walker::walker {
    public:
    bool walk_apply(const SeqLib::BamRecord& record);
    void load_intervals(uint32_t pad);
-   void set_binwidth(uint32_t binwidth);
    void walk_all();
-   uint32_t n_overlap(const uint32_t binstart, uint32_t binend, uint32_t start, uint32_t end);
+   uint32_t n_overlap(const SeqLib::GenomicRegion& region, uint32_t start, uint32_t end);
 
    cc_walker(const std::string& bam_in, const std::string& interval_list) : walker(bam_in), interval_list_path(interval_list) {}
 
@@ -38,6 +37,28 @@ class cc_walker : public walker::walker {
    size_t cur_region_idx = 0;
    SeqLib::GenomicRegion cur_region;
    uint32_t pad;
+
+   std::unordered_map<std::string, read_boundary_t> read_cache;
+
+   uint32_t curstart = 0;
+   uint32_t curend = 0;
+   uint16_t curchr = 0;
+};
+
+
+
+class cc_bin_walker : public walker::walker {
+   public:
+   bool walk_apply(const SeqLib::BamRecord& record);
+   void load_intervals(uint32_t pad);
+   void set_binwidth(uint32_t binwidth);
+   void walk_all();
+   uint32_t n_overlap(const uint32_t binstart, uint32_t binend, uint32_t start, uint32_t end);
+
+   cc_bin_walker(const std::string& bam_in, const uint32_t binwidth) : walker(bam_in), binwidth(binwidth) {}
+
+   protected:
+   target_counts_t target_coverage = {0, 0};
 
    std::unordered_map<std::string, read_boundary_t> read_cache;
    std::unordered_map<uint64_t, target_counts_t> active_bins;
