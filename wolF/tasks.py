eca6f64ad7a53c40e306b665865aa618954f517a
Julian Hess <jhess@broadinstitute.org>
2021-11-08 18:46:58 -0500

    Add wolF task

--- wolF/tasks.py
+++ wolF/tasks.py
@@ -1,35 +1,16 @@
 from wolf import Task
 
-def task1(
-  <task_arg1> = <arg1_default>,
-  <task_arg2> = <arg2_default>,
-  ...,
-  **task_kwargs
-):
-    # preprocessing logic can go here
-
-    return Task(
-      name = <task name>,
-      inputs = {
-        "arg1" : <task_arg1>,
-        "arg2" : <task_arg2>,
-        ...
-      },
-      script = [
-        """
-        # bash script goes here!
-        # $arg1 and $arg2 are available as variables to use, e.g.
-        echo $arg1 $arg2
-        """
-      ],
-      outputs = {
-        "output1" : "<output1 pattern>",
-        "output2" : "<output2 pattern>",
-        ...
-      },
-      docker = "gcr.io/broad-getzlab-workflows/<container name>:<container version>",
-      resources = { "mem" : "1G" },
-      **task_kwargs
-    )
-
-# define additional tasks in the same way that task1 is defined above.
+class Covcollect(Task):
+    inputs = {
+      "bam" : None,
+      "bai" : None,
+      "intervals" : None, # path to BED file or value for even bins
+      "subset_chr" : -1,
+      "subset_start" : 0,
+      "subset_end" : 0
+    }
+    script = """
+    /app/covcollect -b ${bam} -i ${intervals} -o coverage.bed -c ${subset_chr} -s ${subset_start} -e ${subset_end}
+    """
+    output_patterns = { "coverage" : "coverage.bed" }
+    docker = "gcr.io/broad-getzlab-workflows/covcollect:v172"
