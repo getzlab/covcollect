362b389f438e80f5741ab31aa13d745fd0e5f194
Julian Hess <jhess@broadinstitute.org>
2021-11-30 15:17:58 -0500

    < -> <=

--- src/covcollect.cpp
+++ src/covcollect.cpp
@@ -185,7 +185,7 @@ bool cc_bin_walker::walk_apply(const SeqLib::BamRecord &record) {
    binmax = MAX(binmax, (record.Position() / binwidth) * binwidth);
 
    // Add bins
-   for (uint64_t i = binmax; i < record.PositionEnd(); i = i + binwidth) {
+   for (uint64_t i = binmax; i <= record.PositionEnd(); i = i + binwidth) {
        active_bins.emplace(i, (target_counts_t ) { 0, 0 });
    }
 
